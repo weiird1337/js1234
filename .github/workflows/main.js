bf52c9b4a8a6b0814cf81f44e158652c99d4c844
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-23 22:48:17 +0300

    1

---
+++ .github/workflows/main.js
@@ -0,0 +1,21 @@
+var price = 28.99;
+var discount = 10;
+var total =
+ price - (price * (discount / 100));
+if (total > 25) {
+ freeShipping();
+}
+var count = 10;
+while (count > 0) {
+ juggle();
+ count = count - 1;
+}
+var dog = {name: "Rover", weight: 35};
+if (dog.weight > 30) {
+ alert("WOOF WOOF");
+} else {
+ alert("woof woof");
+}
+var circleRadius = 20;
+var circleArea =
+ Math.PI * (circleRadius * circleRadius);
