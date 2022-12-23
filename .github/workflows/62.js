c1ece6da5e154b6c5f3aaa99eaaaedf274943ecf
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 01:46:28 +0300

    Create 62.js

---
+++ .github/workflows/62.js
@@ -0,0 +1,8 @@
+var name = "Jenny";
+ var phone = "867-5309";
+ var fact = "This is a prime number";
+ var songName = phone + "/" + name;
+ var index = phone.indexOf("-");
+ if (fact.substring(10, 15) === "prime") {
+ alert(fact);
+ }
