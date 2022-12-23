4235636f74fb271ab04e48a39bab677d38eda4d6
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 02:39:06 +0300

    Create 142.js

---
+++ .github/workflows/142.js
@@ -0,0 +1,6 @@
+var taxi = new CarPrototype();
+taxi.model = "Delorean Remake";
+taxi.color = "silver";
+taxi.currentTime = new Date();
+taxi.fluxCapacitor = {type: "Mr. Fusion"};
+taxi.timeTravel = function(date) {...};
