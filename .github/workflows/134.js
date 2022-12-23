9f4d5cdfe1629984c17ad9fceac9c57db1dcffff
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 02:36:07 +0300

    Create 134.js

---
+++ .github/workflows/134.js
@@ -0,0 +1,22 @@
+// aint cool
+// var cadi = new Car("GM", "Cadillac", 1955, "tan", 5, false, 12892);
+// var cadiParams = {make: "GM",
+//  model: "Cadillac",
+//  year: 1955,
+//  color: "tan",
+//  passengers: 5,
+//  convertible: false,
+//  mileage: 12892}
+
+
+// cool
+
+var cadiParams = {make: "GM",
+ model: "Cadillac",
+ year: 1955,
+ color: "tan",
+ passengers: 5,
+ convertible: false,
+ mileage: 12892};
+
+var cadi = new Car(cadiParams);
