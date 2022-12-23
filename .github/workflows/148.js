6e3a3beb7ca9ab284854820930992ef2ccf82910
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 02:42:28 +0300

    Create 148.js

---
+++ .github/workflows/148.js
@@ -0,0 +1,16 @@
+var fido = new Dog("Fido", "Mixed", 38);
+if (fido instanceof Dog) {
+ console.log("Fido is a Dog");
+}
+if (fido instanceof ShowDog) {
+ console.log("Fido is a ShowDog");
+}
+var scotty = new ShowDog("Scotty", "Scottish Terrier", 15, "Cookie");
+if (scotty instanceof Dog) {
+ console.log("Scotty is a Dog");
+}
+if (scotty instanceof ShowDog) {
+ console.log("Scotty is a ShowDog");
+}
+console.log("Fido constructor is " + fido.constructor);
+console.log("Scotty constructor is " + scotty.constructor);
