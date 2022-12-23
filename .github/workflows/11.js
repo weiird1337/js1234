1fc49311ae6d5e0370dec8c0be4e8a62b254f258
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 01:11:09 +0300

    Create 11.js

---
+++ .github/workflows/11.js
@@ -0,0 +1,22 @@
+// Test for jokes
+var joke = "JavaScript walked into a bar....';
+var toldJoke = "false";
+var $punchline =
+ "Better watch out for those semi-colons."
+var %entage = 20;
+var result
+if (toldJoke == true) {
+ Alert($punchline);
+} else
+ alert(joke);
+}
+
+\\ Movie Night
+var zip code = 98104;
+var joe'sFavoriteMovie = Forbidden Planet;
+var movieTicket$ = 9;
+if (movieTicket$ >= 9) {
+ alert("Too much!");
+} else {
+ alert("We're going to see " + joe'sFavoriteMovie);
+}
