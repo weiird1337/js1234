1311faf6657d9bd15bed38676ea7b60a3e1ac586
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 02:20:32 +0300

    Create 102.js

---
+++ .github/workflows/102.js
@@ -0,0 +1,23 @@
+var migrating = true;
+var fly = function(num) {
+ var sound = "Flying";
+ function wingFlapper() {
+ console.log(sound);
+ }
+ for (var i = 0; i < num; i++) {
+ wingFlapper();
+ }
+};
+function quack(num) {
+ var sound = "Quack";
+ var quacker = function() {
+ console.log(sound);
+ };
+ for (var i = 0; i < num; i++) {
+ quacker();
+ }
+}
+if (migrating) {
+ quack(4);
+ fly(4);
+}
