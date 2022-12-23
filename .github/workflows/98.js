6ad97857277d26ca2bcda076bbd47cd62e09a4b7
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 02:18:50 +0300

    Create 98.js

---
+++ .github/workflows/98.js
@@ -0,0 +1,18 @@
+function fun(echo) {
+ console.log(echo);
+};
+
+fun("hello");
+function boo(aFunction) {
+ aFunction("boo");
+}
+boo(fun);
+console.log(fun);
+fun(boo);
+var moreFun = fun;
+moreFun("hello again");
+function echoMaker() {
+ return fun;
+}
+var bigFun = echoMaker();
+bigFun("Is there an echo?");
