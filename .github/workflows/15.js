b56b6c2116bce22d8c1eab5e191559b566fc40e9
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 01:15:27 +0300

    Create 15.js

---
+++ .github/workflows/15.js
@@ -0,0 +1,28 @@
+var dogName = "rover";
+var dogWeight = 23;
+if (dogWeight > 20) {
+ console.log(dogName + " says WOOF WOOF");
+} else {
+ console.log(dogName + " says woof woof");
+}
+dogName = "spot";
+dogWeight = 13;
+if (dogWeight > 20) {
+ console.log(dogName + " says WOOF WOOF");
+} else {
+ console.log(dogName + " says woof woof");
+}
+dogName = "spike";
+dogWeight = 53;
+if (dogWeight > 20) {
+ console.log(dogName + " says WOOF WOOF");
+} else {
+ console.log(dogName + " says woof woof");
+}
+dogName = "lady";
+dogWeight = 17;
+if (dogWeight > 20) {
+ console.log(dogName + " says WOOF WOOF");
+} else {
+ console.log(dogName + " says woof woof");
+}
