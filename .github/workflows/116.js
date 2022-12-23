c7f21a8d012a1620163242e83ff7359ef121a065
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 02:26:59 +0300

    Create 116.js

---
+++ .github/workflows/116.js
@@ -0,0 +1,8 @@
+makePassword(password) {
+ return function guess(passwordGuess) {
+ return (passwordGuess === password);
+ };
+}
+var tryGuess = makePassword("secret");
+console.log("Guessing 'nope': " + tryGuess("nope"));
+console.log("Guessing 'secret': " + tryGuess("secret"));
