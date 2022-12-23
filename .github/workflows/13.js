c3b1ef64e7e7f3306239bc236d9b8fc8b2243253
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 01:14:23 +0300

    Create 13.js

---
+++ .github/workflows/13.js
@@ -0,0 +1,16 @@
+//bob
+
+if (price < 200 || price > 600) {
+ alert("Price is too low or too high! Don't buy the gadget.");
+} else {
+ alert("Price is right! Buy the gadget.");
+}
+
+
+//bill
+
+if (price >= 200 || price <= 600) {
+ alert("Price is right! Buy the gadget.");
+} else {
+ alert("Price is too low or too high! Don't buy the gadget.");
+}
