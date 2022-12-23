a21fd964f89fb99e8b16abb131e148ecbe1a88c1
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 02:31:28 +0300

    Create 125.js

---
+++ .github/workflows/125.js
@@ -0,0 +1,18 @@
+(function(food) {
+ if (food === "cookies") {
+ alert("More please");
+ } else if (food === "cake") {
+ alert("Yum yum");
+ }
+ })("cookies");
+
+
+var eat = function(food) {
+ if (food === "cookies") {
+ alert("More please");
+ } else if (food === "cake") {
+ alert("Yum yum");
+ }
+};
+(eat)("cookies");
+
