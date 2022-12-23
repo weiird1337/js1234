6cd874a9f1e02b512bc19877527c589f62fa2dc8
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 01:30:24 +0300

    Create 43.js

---
+++ .github/workflows/43.js
@@ -0,0 +1,19 @@
+// objects
+
+var chevy = {
+ make: "Chevy",
+ model: "Bel Air",
+ year: 1957,
+ color: "red",
+ passengers: 2,
+ convertible: false,
+ mileage: 1021
+  
+  
+  
+  //
+  
+  var miles = fiat.mileage;
+if (miles < 2000) {
+ buyIt();
+}
