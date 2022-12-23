33fd906a9715d20c40b4f8ee54feed0f6a097a36
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 02:09:39 +0300

    Create 84.js

---
+++ .github/workflows/84.js
@@ -0,0 +1,17 @@
+function quack(num) {
+ for (var i = 0; i < num; i++) {
+ console.log("Quack!");
+ }
+}
+quack(3);
+
+
+//
+
+var fly = function(num) {
+ for (var i = 0; i < num; i++) {
+ console.log("Flying!");
+ }
+};
+fly(3);
+
