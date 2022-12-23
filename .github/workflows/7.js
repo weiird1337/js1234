7098f7277d8b76330415ea7b6160e1596d44bd13
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-23 22:55:07 +0300

    Create 7.js

---
+++ .github/workflows/7.js
@@ -0,0 +1,13 @@
+if (scoops >= 5) {
+ alert("Eat faster, the ice cream is going to melt!");
+} else if (scoops == 3) {
+ alert("Ice cream is running low!");
+} else if (scoops == 2) {
+ alert("Going once!");
+} else if (scoops == 1) {
+ alert("Going twice!");
+} else if (scoops == 0) {
+ alert("Gone!");
+} else {
+ alert("Still lots of ice cream left, come and get it.");
+}
