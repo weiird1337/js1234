1e8f17e83fde3733834332b7e434ea7d81b372fe
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 02:16:30 +0300

    Create 93.js

---
+++ .github/workflows/93.js
@@ -0,0 +1,13 @@
+var numbersArray = [60, 50, 62, 58, 54, 54];
+
+function compareNumbers(num1, num2) {
+ if (num1 > num2) {
+ return 1;
+ } else if (num1 === num2) {
+ return 0;
+ } else {
+ return -1;
+ }
+}
+
+console.log(numbersArray);
