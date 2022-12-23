638588190474f3681fb37e6e9a43e4f624138b02
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 02:33:16 +0300

    Create 126.js

---
+++ .github/workflows/126.js
@@ -0,0 +1,15 @@
+var taxi = {
+ make: "Webville Motors",
+ model: "Taxi",
+ year: 1955,
+ color: "yellow",
+ passengers: 4,
+ convertible: false,
+ mileage: 281341,
+ started: false,
+ start: function() { this.started = true;},
+ stop: function() { this.started = false;},
+ drive: function {
+ // Код управления машиной
+ }
+};
