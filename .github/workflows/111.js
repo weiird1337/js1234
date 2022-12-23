050163802e0da869f9bf7a5f7809e96d723659ea
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 02:24:59 +0300

    Create 111.js

---
+++ .github/workflows/111.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+ var count = 0;
+ var message = "You clicked me ";
+ var div = document.getElementById("message");
+ var button = document.getElementById("clickme");
+ button.onclick = function() {
+ count++;
+ div.innerHTML = message + count + " times!";
+ };
+};
