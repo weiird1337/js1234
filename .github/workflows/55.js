68a42efb45ce895e02a85d244169c361b7d1d4c7
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 01:42:03 +0300

    Create 55.js

---
+++ .github/workflows/55.js
@@ -0,0 +1,13 @@
+var x;
+if (x == undefined) {
+ // Переменная x не инициализирована! Принять меры!
+}
+
+
+
+var customer = {
+ name: "Jenny"
+};
+if (customer.phoneNumber == undefined) {
+ // Получить телефон клиента
+}
