cb5cb417d189c371f64271a1640e9db580030556
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-23 22:51:56 +0300

    Create 4.js

---
+++ .github/workflows/4.js
@@ -0,0 +1,11 @@
+var joke = "JavaScript walked into a bar....';
+var toldJoke = "false";
+var $punchline =
+ "Better watch out for those semi-colons."
+var %entage = 20;
+var result
+if (toldJoke == true) {
+ Alert($punchline);
+} else
+ alert(joke);
+}
