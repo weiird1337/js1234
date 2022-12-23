eea515919866ebd46d9500dcceb08a6a4ea1c6b7
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 01:24:53 +0300

    Create 34.js

---
+++ .github/workflows/34.js
@@ -0,0 +1,14 @@
+// arrays
+ var genres = ["80s", "90s", "Electronic", "Folk"];
+//
+var genres = [];
+//
+// var genres = [];
+//  genres[0] = "Rockabilly";
+//  genres[1] = "Ambient";
+//  var size = genres.length;
+//
+var genres = [];
+ genres.push("Rockabilly");
+ genres.push("Ambient");
+ var size = genres.length;
