937ce89824a14bb717aa6bdd9140a6090bf28cd7
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 01:19:44 +0300

    Create 24.js

---
+++ .github/workflows/24.js
@@ -0,0 +1,27 @@
+var avatar = "generic";
+var skill = 1.0;
+var pointsPerLevel = 1000;
+var userPoints = 2008;
+
+function getAvatar(points) {
+ var level = points / pointsPerLevel;
+ if (level == 0) {
+ return "Teddy bear";
+ } else if (level == 1) {
+ return "Cat";
+ } else if (level >= 2) {
+ return "Gorilla";
+ }
+}
+
+function updatePoints(bonus, newPoints) {
+ var i = 0;
+ while (i < bonus) {
+ newPoints = newPoints + skill * bonus;
+ i = i + 1;
+ }
+ return newPoints + userPoints;
+}
+
+userPoints = updatePoints(2, 100);
+avatar = getAvatar(2112);
