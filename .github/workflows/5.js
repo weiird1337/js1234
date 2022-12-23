859886abfa2ad8d776339949f3d0d8c2fca945ed
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-23 22:52:22 +0300

    Create 5.js

---
+++ .github/workflows/5.js
@@ -0,0 +1,8 @@
+var zip code = 98104;
+var joe'sFavoriteMovie = Forbidden Planet;
+var movieTicket$ = 9;
+if (movieTicket$ >= 9) {
+ alert("Too much!");
+} else {
+ alert("We're going to see " + joe'sFavoriteMovie);
+}
