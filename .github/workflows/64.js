f5689256eaca69662a58d2b434e5f7f70e69b8b0
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 01:47:20 +0300

    Create 64.js

---
+++ .github/workflows/64.js
@@ -0,0 +1,10 @@
+var phrase = "the cat in the hat";
+
+var index = phrase.indexOf("cat");
+console.log("there's a cat sitting at index " + index);
+
+index = phrase.indexOf("the", 5);
+console.log("there's a the sitting at index " + index);
+
+index = phrase.indexOf("dog");
+console.log("there's a dog sitting at index " + index);
