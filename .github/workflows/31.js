c0ab448b7c08b081ddd5c53c0c7ac5eecc11611c
weiird1337 <117383884+weiird1337@users.noreply.github.com>
2022-12-24 01:22:27 +0300

    Create 31.js

---
+++ .github/workflows/31.js
@@ -0,0 +1,27 @@
+function dogYears(dogName, age) {
+ var years = age * 7;
+ console.log(dogName + " is " + years + " years old");
+}
+var myDog = "Fido";
+dogYears(myDog, 4);
+function makeTea(cups, tea) {
+ console.log("Brewing " + cups + " cups of " + tea);
+}
+var guests = 3;
+makeTea(guests, "Earl Grey");
+function secret() {
+ console.log("The secret of life is 42");
+}
+secret();
+function speak(kind) {
+ var defaultSound = "";
+ if (kind == "dog") {
+ alert("Woof");
+ } else if (kind == "cat") {
+ alert("Meow");
+ } else {
+ alert(defaultSound);
+ }
+}
+var pet = prompt("Enter a type of pet: ");
+speak(pet);
